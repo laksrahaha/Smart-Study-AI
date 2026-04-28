40e8d0bf2f9af725a0f9b43048dff92d335f85c0
SLove218 <louisewang002@gmail.com>
2026-04-29 01:05:44 +1200

    Update Program.cs
    link to login page

--- backend/SmartStudyAI.Backend/Program.cs
+++ backend/SmartStudyAI.Backend/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseCors("AllowFrontend");
+app.MapControllers();
 app.Run();
